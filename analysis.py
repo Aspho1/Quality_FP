b69621d6c02060de32ef674883deeaa23bea0573
Caleb <caleb.robison3@gmail.com>
2024-04-28 08:09:48 -0600

    liug2

--- analysis.py
+++ analysis.py
@@ -1,38 +1,48 @@
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
 
-from sklearn.metrics import accuracy_score, calinski_harabasz_score
+from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import StratifiedKFold, GridSearchCV, train_test_split
-from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
 
-def try_RFC_KFolds(X, y, rfc):
 
-    kfolds=StratifiedKFold(n_splits=10)
+def std(X):
 
-    pipe = make_pipeline(rfc, )
+    for i in range(len(X.columns)):
+        target_column = X.columns[i]
+        X[target_column] = (X[target_column] - X[target_column].mean()) / X[target_column].std()
+    return X
 
-    fold_num = 1
-    for train_index, test_index in kfolds.split(X,y):
-        X_train, X_test = X.loc[train_index], X.loc[test_index]
-        y_train, y_test = y[train_index], y[test_index]
-        pipe.fit(X_train,y_train)
-        y_pred = pipe.predict(X_test)
 
-        acc = accuracy_score(y_test, y_pred)
-        # cal = calinski_harabasz_score(y_test, y_pred)
-        print(f"Accuracy for fold {fold_num}: ", acc)
-        # print(f"Accuracy for fold {fold_num}: ", cal)
-        fold_num += 1
+def pca_f(X, y):
 
+    target_names = y
+
+    pca = PCA(n_components=2)
+    X_r = pca.fit(X).transform(X)
+
+    # Percentage of variance explained for each components
+    print("explained variance ratio (first two components): %s"% str(pca.explained_variance_ratio_))
+
+
+    plt.figure()
+    colors = ["navy", "turquoise", "darkorange"]
+    lw = 2
+
+    for color, i, target_name in zip(colors, [1, 2, 3], target_names):
+        plt.scatter(
+            X_r[y == i, 0], X_r[y == i, 1], color=color, alpha=0.4, lw=lw, label=target_name
+        )
+    plt.legend(loc="best", shadow=False, scatterpoints=1)
+    plt.title("PCA of NASA dataset")
+    plt.show()
 
 def try_RFC(X, y, rfc):
-
-    pipe = make_pipeline(rfc)
-
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
-    pipe.fit(X_train,y_train)
-    y_pred = pipe.predict(X_test)
+    rfc.fit(X_train, y_train)
+    y_pred = rfc.predict(X_test)
 
     acc = accuracy_score(y_test, y_pred)
     # cal = calinski_harabasz_score(y_test, y_pred)
@@ -43,43 +53,14 @@ def try_RFC(X, y, rfc):
 
 
 
-
-def grid_search_parameters(X,y, rfc):
-    print("performing a grid_search for optimal RFC parameters based on AUC Score")
-
-    param_grid = {
-
-        'n_estimators': [100, 200, 300], 
-        # 'n_estimators': [100 , 200], 
-        'max_features': ['sqrt', None],
-        # 'max_features': ['sqrt'],
-        # 'bootstrap': [True],
-        'bootstrap': [True, False],
-        'criterion': ['gini', 'entropy'],
-        'max_depth': [None, 10, 20, 30],
-        'min_samples_leaf': [1, 2, 3],
-        # 'min_samples_leaf': [1],
-    }
-
-    print(f"    Included parameters: {param_grid}")
-    grid_search = GridSearchCV(rfc, param_grid, scoring='rand_score', cv=5)
-    grid_search.fit(X, y)
-
-    # Get the best model and AUC score
-    best_model = grid_search.best_estimator_
-    best_score = grid_search.best_score_
-
-    # Output the best score
-    print(best_model)
-    print(f"best roc_AUC: {best_score:5f}")
-
-
-def load_data(fpath) -> pd.DataFrame:
+def load_data(fpath):
     full_df = pd.read_csv(fpath + "\\star_classification.csv")
-    features = full_df.drop(columns = "class")
-    labels = full_df["class"].map({"GALAXY":1, "QSO":2, "STAR":3}).ravel()
+    full_df = full_df[full_df.u>-999]
+    features = full_df.drop(columns=["class","rerun_ID"])
+    labels = full_df["class"].map({"GALAXY": 1, "QSO": 2, "STAR": 3}).ravel()
 
     return features, labels
+
 
 def main():
     path = os.path.dirname(os.path.abspath(__file__))
@@ -89,14 +70,11 @@ def main():
 
     rfc = RandomForestClassifier(random_state=1)
 
-    grid_search_parameters(X,y,rfc)
+   # try_RFC(X, y, rfc)
 
-    # try_RFC_KFolds(X,y,rfc)
-
-    try_RFC(X,y,rfc)
-
-
-
+    X = std(X)
+    pca_f(X, y)
+    print(X.columns)
 
 
 main()
